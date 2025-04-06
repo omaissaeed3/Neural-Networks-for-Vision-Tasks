0cfb059fe9fcf97807f1a1e9af5c94a6a2851f31
omaissaeed3 <omaissaeed3@gmail.com>
2025-04-05 21:41:25 -0400

    Loaded and preprocessed CIFAR-10 dataset

--- cnn_cifar10.py
+++ cnn_cifar10.py
@@ -1,1 +1,16 @@
-#Installed Libraries
+# Load CIFAR-10 dataset
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+
