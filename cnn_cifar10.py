2e045d625e5ac874e575c02f15d995b5cd9b757f
omaissaeed3 <omaissaeed3@gmail.com>
2025-04-05 21:42:05 -0400

    Implemented CNN for CIFAR-10 classification

--- cnn_cifar10.py
+++ cnn_cifar10.py
@@ -14,3 +14,30 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
 
+# Define a simple CNN
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(32 * 16 * 16, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = x.view(-1, 32 * 16 * 16)
+        x = self.fc1(x)
+        return x
+
+model = CNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+for epoch in range(5):
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+    print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
